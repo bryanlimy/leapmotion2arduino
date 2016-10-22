03c98f4efb9489042af0271b5b88ad896d857099
Bryan Li <bryanlimy@gmail.com>
2016-10-22 18:18:24 -0400

    got LED working

--- leap2arduino.js
+++ leap2arduino.js
@@ -5,15 +5,14 @@ var Cylon = require("cylon");
 Cylon.robot({
 	connections: {
 		leap: { adaptor: "leapmotion" },
-		// arduino: { adaptor: "firmata", port: "/dev/tty.usbmodem1411" }
+		arduino: { adaptor: "firmata", port: "/dev/cu.usbmodem1411" }
 	},
-
 	devices: {
-		leapmotion: {driver: "leapmotion", connection: "leap"},
-		// led: { driver: "led", pin: 13, connection: "arduino" }
+		led: { driver: "led", pin: 13, connection: "arduino" },
+		leapmotion: {driver: "leapmotion", connection: "leap"}
 	},
-
 	work: function(my) {
+
 		my.leapmotion.on('frame', function(frame) {
 			console.log(frame.toString());
 		});
@@ -22,12 +21,12 @@ Cylon.robot({
 			console.log(hand.toString());
 		});
 
-		// my.leapmotion.on("frame", function(frame) {
-		// 	if (frame.hands.length > 0) {
-		// 		my.led.turnOn();
-		// 	} else {
-		// 		my.led.turnOff();
-		// 	}
-		// });
+		my.leapmotion.on("frame", function(frame) {
+			if (frame.hands.length > 0) {
+				my.led.turnOn();
+			} else {
+				my.led.turnOff();
+			}
+		});
 	}
 }).start();
