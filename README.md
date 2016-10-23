d97780485aa12cf034492a08ef9a46774f061199
Bryan Li <bryanlimy@gmail.com>
2016-10-23 12:17:24 -0400

    added directional controls

--- README.md
+++ README.md
@@ -12,8 +12,12 @@ LED get brightened or dimmed as your hand raise or fall
 `brew install npm`
 - [Cylon.js](https://cylonjs.com/):
 `npm install cylon cylon-leapmotion cylon-firmata`
+- [Gort](http://gort.io/):
+`brew install hybridgroup/tools/gort`
 
 
 ### How-to Run
 - Plug-in the Leap Motion Controller and Arduino with LED at pin 3
+- (Optional) Scan Arduino port path, execute `gort scan serial`
+- (Optional) Upload Firmata to Arduino, execute `gort arduino upload firmata </path/to/Arduino>`
 - In terminal, execute `node leap2arduino.js`
--- leap2arduino.js
+++ leap2arduino.js
@@ -5,31 +5,56 @@ var Cylon = require("cylon");
 Cylon.robot({
 	connections: {
 		leap: { adaptor: "leapmotion"},
-		arduino: { adaptor: "firmata", port: "/dev/cu.usbmodem1411"}
+		arduino: { adaptor: "firmata", port: "/dev/tty.usbmodem1411"}
 	},
 	devices: {
-		led: { driver: 'led', pin: 3, connection: "arduino"},
+		front: {driver: "led", pin: 11, connection: "arduino"},
+		back: {driver: "led", pin: 5, connection: "arduino"},
+		left: {driver: "led", pin: 3, connection: "arduino"},
+		right: {driver: "led", pin: 6, connection: "arduino"},
 		leapmotion: {driver: "leapmotion", connection: "leap"}
 	},
 	work: function(my) {
 		var brightness = 0, delta = 2;
 
+		// pitch forward 
 		my.leapmotion.on('hand', function(hand) {
+			console.log("pitch: " + hand.pitch() + ", roll: " + hand.roll());
 
-			var velocity = hand.palmVelocity[1];
-			if (Math.abs(velocity) > 20) {
-				if (velocity > 0) {
-					if (brightness < 255) {
-						brightness += delta;
-					}
-					console.log("Z axis: " + velocity + ", brightening");
+			if (Math.abs(hand.pitch()) > 0.5) {
+				if (hand.pitch() < 0) {
+					// pitch up, go forward
+					my.front.turnOn();
+					my.back.turnOff();
+					my.left.turnOff();
+					my.right.turnOff();
 				} else {
-					if (brightness > 0) {
-						brightness -= delta;
-					}
-					console.log("Z axis: " + velocity + ", dimming");
+					// pitch down, go backward
+					my.front.turnOff();
+					my.back.turnOn();
+					my.left.turnOff();
+					my.right.turnOff();
 				}
-				my.led.brightness(brightness);
+			}  else if (Math.abs(hand.roll()) > 0.5) {
+				if (hand.roll() < 0) {
+					// roll right, go right
+					my.front.turnOff();
+					my.back.turnOff();
+					my.left.turnOff();
+					my.right.turnOn();
+				} else {
+					// roll left, go left
+					my.front.turnOff();
+					my.back.turnOff();
+					my.left.turnOn();
+					my.right.turnOff();
+				}
+			} else {
+				// hand is flat, turn everything off 
+				my.front.turnOff();
+				my.back.turnOff();
+				my.left.turnOff();
+				my.right.turnOff();
 			}
 		});
 	}
